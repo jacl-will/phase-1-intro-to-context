565083cbcb3e2f7678c023832e0964eb29bb5f9f
Jaclyn Williams <jaclyn.williams143@gmail.com>
2023-02-05 19:24:17 -0500

    commit tree f156d4b67c2f205bc218a22ab07d5e23774eb49a

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Your code here
+
+function createEmployeeRecord(employee) {
+    
+    return {
+        firstName: employee[0],
+        familyName: employee[1], 
+        title: employee[2],
+        payPerHour: employee[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+ 
+function createEmployeeRecords(employees) {
+    return employees.map(employee => createEmployeeRecord(employee))
+}
+
+function createTimeInEvent(event) {
+    let [date, hour] = event.split(" ")
+    let eventObj = {
+        typeof: "TimeIn",
+        hour: parseInt(hour, 10),
+        date
+    }
+    employee.timeInEvents.push(eventObj)
+    // employee.timeInEvents.push(eventObj)
+    return employee
+}
+
+function createTimeOutEvent(event) {
+    let [date, hour] = event.split(" ")
+    let eventObj = {
+        typeof: "TimeOut",
+        hour: parseInt(hour, 10),
+        date
+    }
+
+    employee.timeOutEvents.push(eventObj)
+    return employee
+}
